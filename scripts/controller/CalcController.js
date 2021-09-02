61718f8bed98bce5576b872df11280758b34b34c
hugocostav <hugocostam100@outlook.com.br>
2021-09-02 00:56:47 -0300

    added getters and setters

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,11 +6,11 @@ class CalcController {
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
-        this._lacale = 'pt-BR';
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#hora');
-        this,_currentDate;
+        this._currentDate;
         this.initialize();
         this.initButtonsEvents();
         this.initKeyboard();
@@ -294,5 +294,46 @@ class CalcController {
                 year: 'numeric'
             });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+
+        return this._displayCalcEl.innerHTML;
+
+    }
+
+    set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
+
+        this._displayCalcEl.innerHTML = value;
+
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
     }
 }
