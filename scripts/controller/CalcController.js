61a6750d926746c82e7584f96f031c29ae0cc9cc
hugocostav <hugocostam100@outlook.com.br>
2021-09-01 23:30:08 -0300

    added addEventListenerAll

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -101,4 +101,12 @@ class CalcController {
             }
         })
     }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+
 }
