8b403dae2e1199f48a1fa68f9d9742d50384d50c
hugocostav <hugocostam100@outlook.com.br>
2021-09-02 00:52:42 -0300

    added setDisplayDateTime

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -285,4 +285,14 @@ class CalcController {
             });
         });
     }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(
+            this._locale, {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
 }
